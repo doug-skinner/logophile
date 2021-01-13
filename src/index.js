90fc25d1c580260c0417462c089542f5ce41e536
Doug Skinner <me@doug-skinner.com>
2021-01-13 10:33:14 -0500

    update formatting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const { createLogger, format, transports } = require('winston');
+const { createLogger, format, transports } = require('winston')
 
-const { combine, timestamp, json } = format;
+const { combine, timestamp, json } = format
 
-const DEFAULT_LEVEL = 'info';
+const DEFAULT_LEVEL = 'info'
 const logLevels = Object.freeze({
     debug: 4,
     error: 0,
@@ -10,38 +10,32 @@ const logLevels = Object.freeze({
     off: -1,
     trace: 3,
     warn: 1
-});
+})
 
-const GLOBAL_LOG_CONTEXT = {};
+const GLOBAL_LOG_CONTEXT = {}
 
 const globalLogContextKeysFormat = format((info) => ({
     ...info,
     ...GLOBAL_LOG_CONTEXT
-}));
+}))
 
 const errorFormat = () => {
-
     const replaceError = ({ label, level, message, stack }) => ({
         label,
         level,
         message,
         stack
-    });
+    })
     const replacer = (key, value) => {
-
         if (value instanceof Error) {
-
-            return replaceError(value);
-
+            return replaceError(value)
         }
 
-        return value;
+        return value
+    }
 
-    };
-
-    return format.json({ replacer });
-
-};
+    return format.json({ replacer })
+}
 
 const CURRENT_FORMAT = [
     globalLogContextKeysFormat(),
@@ -49,71 +43,58 @@ const CURRENT_FORMAT = [
     timestamp(),
     json(),
     errorFormat()
-];
-const CURRENT_TRANSPORTS = [new transports.Console()];
+]
+const CURRENT_TRANSPORTS = [new transports.Console()]
 
 const CURRENT_CONFIG = {
     format: combine(...CURRENT_FORMAT),
     levels: logLevels,
     transports: CURRENT_TRANSPORTS
-};
-
+}
 
 const initConfig = () => {
-
-    CURRENT_CONFIG.level = CURRENT_CONFIG.level || DEFAULT_LEVEL;
+    CURRENT_CONFIG.level = CURRENT_CONFIG.level || DEFAULT_LEVEL
     CURRENT_TRANSPORTS.forEach((transport) => {
-
-        transport.level = CURRENT_CONFIG.level;
-
-    });
-
-};
+        transport.level = CURRENT_CONFIG.level
+    })
+}
 
 const getLoggerImpl = () => {
+    initConfig()
+    const loggerImpl = createLogger(CURRENT_CONFIG)
 
-    initConfig();
-    const loggerImpl = createLogger(CURRENT_CONFIG);
-
-    return loggerImpl;
-
-};
+    return loggerImpl
+}
 
 const setLogLevel = (level) => {
-
     CURRENT_CONFIG.level = level
         ? level.toLowerCase()
-        : DEFAULT_LEVEL;
+        : DEFAULT_LEVEL
     CURRENT_TRANSPORTS.forEach((transport) => {
+        transport.level = CURRENT_CONFIG.level
+    })
+    getLoggerImpl().configure(CURRENT_CONFIG)
+}
 
-        transport.level = CURRENT_CONFIG.level;
-
-    });
-    getLoggerImpl().configure(CURRENT_CONFIG);
-
-};
-
-const getLogLevel = () => CURRENT_CONFIG.level || DEFAULT_LEVEL;
+const getLogLevel = () => CURRENT_CONFIG.level || DEFAULT_LEVEL
 
 const addGlobalLogContextKeys = (keys = {}) => {
-
     Object.assign(
         GLOBAL_LOG_CONTEXT,
         keys
-    );
-    getLoggerImpl().configure(CURRENT_CONFIG);
+    )
+    getLoggerImpl().configure(CURRENT_CONFIG)
+}
 
-};
-
-const error = (...args) => getLoggerImpl().error(...args);
-const warn = (...args) => getLoggerImpl().warn(...args);
-const info = (...args) => getLoggerImpl().info(...args);
-const debug = (...args) => getLoggerImpl().debug(...args);
-const trace = (...args) => getLoggerImpl().trace(...args);
+const error = (...args) => getLoggerImpl().error(...args)
+const warn = (...args) => getLoggerImpl().warn(...args)
+const info = (...args) => getLoggerImpl().info(...args)
+const debug = (...args) => getLoggerImpl().debug(...args)
+const trace = (...args) => getLoggerImpl().trace(...args)
 
 // Necessary for output when you don't want to include the context keys
 // eslint-disable-next-line no-console
-const log = (...args) => console.log(...args);
+const log = (...args) => console.log(...args)
 
 module.exports = {
     addGlobalLogContextKeys,
@@ -125,4 +106,4 @@ module.exports = {
     setLogLevel,
     trace,
     warn
-};
+}
